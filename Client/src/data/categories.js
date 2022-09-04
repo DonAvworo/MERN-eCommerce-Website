d5906ff1c0f55754533d4093babc3475c9b98734
Don Avworo <101209035+DonAvworo@users.noreply.github.com>
2022-09-04 21:10:01 +0100

    pulled from main collaborative page

---
+++ Client/src/data/categories.js
@@ -0,0 +1,28 @@
+const categories = [
+  {
+    "name": "a painting of the pyramids surrounded by forest"
+  },
+  {
+    "name": "zebras wearing suits smoking cigars"
+  },
+  {
+    "name": "japanese calligraphy of black and white koi"
+  },
+  {
+    "name": "surrealist oil painting by Salvador Dalí of a cow eating cheese"
+  },
+  {
+    "name": "giant wolf in space"
+  },
+  {
+    "name": "stained glass window depicting a cheeseburger"
+  },
+  {
+    "name": "a green ogre playing chess with a blue dinosaur"
+  },
+  {
+    "name": "photorealistic black and white cat looking through the window on a rainy night"
+  },
+]
+
+export default categories;
